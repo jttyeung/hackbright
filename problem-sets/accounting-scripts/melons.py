e907e027502e151be0d82ca0dc0459b8bbd34bbf
Joanne Yeung <jttyeung@gmail.com>
2017-01-09 20:30:15 -0800

    reformatted melon dictionary

--- problem-sets/accounting-scripts/melons.py
+++ problem-sets/accounting-scripts/melons.py
@@ -1,37 +1,37 @@
 melons = {
-    "Honeydew": {
-        price: 0.99,
-        seedless = True,
-        flesh_color: None,
-        weight: None,
-        rind: None
+    'Honeydew': {
+        'price': 0.99,
+        'seedless': True,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
         },
-    "Crenshaw": {
-        price: 2.00,
-        seedless = False,
-        flesh_color: None,
-        weight: None,
-        rind: None
+    'Crenshaw': {
+        'price': 2.00,
+        'seedless': False,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
         },
-    "Crane": {
-        price: 2.50,
-        seedless = False,
-        flesh_color: None,
-        weight: None,
-        rind: None
+    'Crane': {
+        'price': 2.50,
+        'seedless': False,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
         },
-    "Casaba": {
-        price: 2.50,
-        seedless = False,
-        flesh_color: None,
-        weight: None,
-        rind: None
+    'Casaba': {
+        'price': 2.50,
+        'seedless': False,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
         },
-    "Cantaloupe": {
-        price: 0.99,
-        seedless = False,
-        flesh_color: None,
-        weight: None,
-        rind: None
+    'Cantaloupe': {
+        'price': 0.99,
+        'seedless': False,
+        'flesh_color': None,
+        'weight': None,
+        'rind_color': None
         }
 }
