c5986cf5f61fc13eb13e95928211c792d3e4c964
Joanne Yeung <jttyeung@gmail.com>
2017-01-09 20:31:08 -0800

    refactored to support unlimited attributes

--- problem-sets/accounting-scripts/melon_info.py
+++ problem-sets/accounting-scripts/melon_info.py
@@ -2,16 +2,37 @@
 Prints out all the melons in our inventory
 """
 
-from melons import melon_names, melon_seedlessness, melon_prices
+from melons import melons
 
 
-def print_melon(name, seedless, price):
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
+# def print_melon(melon_data):
+#     """
+#     Returns the charateristics and price of the melon.
+#     """
+#     have_or_have_not = 'have'
+#     if melons[melon_name]['seedless']:
+#         have_or_have_not = 'do not have'
 
-    print "{}s {} seeds and are ${:.2f}".format(name, have_or_have_not, price)
+#     price = melons[melon_name]['price']
+#     flesh_color = melons[melon_name]['flesh_color']
+#     weight = melons[melon_name]['weight']
+#     rind_color = melons[melon_name]['rind_color']
+
+#     print "{}s {} seeds, have {} flesh, have a {} rind, weigh {}, and are ${:.2f}".format(melon_name, have_or_have_not, flesh_color, rind_color, weight, price)
 
 
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+# for melon in melons:
+#     print_melon(melon)
+
+
+def print_melons(melon_data):
+    """
+    Returns the charateristics and price of the melon.
+    """
+    for melon, attributes in melon_data.items():
+        print
+        print melon
+        for attribute, value in attributes.items():
+            print "{}: {}".format(attribute, value)
+
+print_melons(melons)
