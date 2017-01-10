cf6e9426e063a9170defd216fa8b1075cf149202
Joanne Yeung <jttyeung@gmail.com>
2017-01-09 19:46:50 -0800

    refactored melon dictionary

--- problem-sets/accounting-scripts/melons.py
+++ problem-sets/accounting-scripts/melons.py
@@ -1,23 +1,36 @@
-melon_names = {
-    1: "Honeydew",
-    2: "Crenshaw",
-    3: "Crane",
-    4: "Casaba",
-    5: "Cantaloupe",
-}
-
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
-
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
+melons = {
+    "Honeydew": {
+        price: 0.99,
+        seedless = True,
+        flesh_color: None,
+        weight: None,
+        rind: None
+        },
+    "Crenshaw": {
+        price: 2.00,
+        seedless = False,
+        flesh_color: None,
+        weight: None,
+        rind: None
+        },
+    "Crane": {
+        price: 2.50,
+        seedless = False,
+        flesh_color: None,
+        weight: None,
+        rind: None
+        },
+    "Casaba": {
+        price: 2.50,
+        seedless = False,
+        flesh_color: None,
+        weight: None,
+        rind: None
+        },
+    "Cantaloupe": {
+        price: 0.99,
+        seedless = False,
+        flesh_color: None,
+        weight: None, rind: None
+        }
 }
