d971d9375d1d4a407d5debc86809d8fdfc770a8b
Joanne Yeung <jttyeung@gmail.com>
2017-01-14 21:45:38 +0000

    added object orientation exercises

---
+++ problem-sets/object-orientation/assessment.py
@@ -0,0 +1,23 @@
+"""
+Part 1: Discussion
+
+1. What are the three main design advantages that object orientation
+   can provide? Explain each concept.
+
+2. What is a class?
+
+3. What is an instance attribute?
+
+4. What is a method?
+
+5. What is an instance in object orientation?
+
+6. How is a class attribute different than an instance attribute?
+   Give an example of when you might use each.
+
+
+"""
+
+
+# Parts 2 through 5:
+# Create your classes and class methods
