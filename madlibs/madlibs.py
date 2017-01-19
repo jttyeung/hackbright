af82a2237cf6ffa3c73104ac79362bda29718da8
Joanne Yeung <jttyeung@gmail.com>
2017-01-18 22:08:36 -0800

    added link to hilairous sample madlib

--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -51,14 +51,62 @@ def show_madlib_form():
 @app.route('/madlib')
 def show_madlib():
 
-    person = request.args.get("person")
-    color = request.args.get("color")
+    verbing = request.args.get("verbing")
+    place = request.args.get("place")
+    holiday_song = request.args.get("holiday_song")
+    adjective1 = request.args.get("adjective1")
+    adjective2 = request.args.get("adjective2")
+    name1 = request.args.get("name1")
+    verbed1 = request.args.get("verbed1")
+    verbed2 = request.args.get("verbed2")
+    amount_of_time1 = request.args.get("amount_of_time1")
+    verb = request.args.get("verb")
+    plural_noun = request.args.get("plural_noun")
+    name2 = request.args.get("name2")
+    number = request.args.get("number")
+    food = request.args.get("food")
+    verbed3 = request.args.get("verbed3")
     noun = request.args.get("noun")
-    adjective = request.args.get("adjective")
-    places = request.args.getlist("places")
+    singer = request.args.get("singer")
+    adjective3 = request.args.get("adjective3")
+    name3 = request.args.get("name3")
+    messy_food = request.args.get("messy_food")
+    body_part = request.args.get("body_part")
+    amount_of_time2 = request.args.get("amount_of_time2")
+    holiday_food = request.args.get("holiday_food")
 
-    return render_template("madlib.html", person=person, color=color,
-                           noun=noun, adjective=adjective, places=places)
+    print request.args
+
+    return render_template("madlib.html",
+                           verbing=verbing,
+                           place=place,
+                           holiday_song=holiday_song,
+                           adjective1=adjective1,
+                           adjective2=adjective2,
+                           name1=name1,
+                           verbed1=verbed1,
+                           verbed2=verbed2,
+                           amount_of_time1=amount_of_time1,
+                           verb=verb,
+                           plural_noun=plural_noun,
+                           name2=name2,
+                           number=number,
+                           food=food,
+                           verbed3=verbed3,
+                           noun=noun,
+                           singer=singer,
+                           adjective3=adjective3,
+                           name3=name3,
+                           messy_food=messy_food,
+                           body_part=body_part,
+                           amount_of_time2=amount_of_time2,
+                           holiday_food=holiday_food,
+                           )
+
+
+@app.route("/sample")
+def show_sample():
+    return render_template("sample_madlib.html")
 
 
 if __name__ == '__main__':
