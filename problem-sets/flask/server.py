0a5a89ddc30278d46457d198beff6f8dc374a2a9
Joanne Yeung <jttyeung@gmail.com>
2017-02-04 16:06:49 -0800

    updated spacing from 2 to 4 spaces

--- problem-sets/flask/server.py
+++ problem-sets/flask/server.py
@@ -12,23 +12,23 @@ app.secret_key = "ABC"
 # YOUR ROUTES GO HERE
 @app.route('/')
 def homepage():
-  """Show homepage"""
-  return render_template('index.html')
+    """Show homepage"""
+    return render_template('index.html')
 
 @app.route('/application-form')
 def application_form():
-  """Show application form"""
-  positions = ["Software Engineer", "QA Engineer", "Product Manager", "Kitteh Master", "Furball Remover"]
-  return render_template('application-form.html', positions=positions)
+    """Show application form"""
+    positions = ["Software Engineer", "QA Engineer", "Product Manager", "Kitteh Master", "Furball Remover"]
+    return render_template('application-form.html', positions=positions)
 
 @app.route('/application-success', methods=['POST'])
 def application_success():
-  """Show application submit success"""
-  firstname = request.form.get('firstname')
-  lastname = request.form.get('lastname')
-  position = request.form.get('position')
-  salaryrequirement = int(request.form.get('salaryreq'))
-  return render_template('application-response.html', firstname=firstname, lastname=lastname, jobtitle=position, salaryrequirement=salaryrequirement)
+    """Show application submit success"""
+    firstname = request.form.get('firstname')
+    lastname = request.form.get('lastname')
+    position = request.form.get('position')
+    salaryrequirement = int(request.form.get('salaryreq'))
+    return render_template('application-response.html', firstname=firstname, lastname=lastname, jobtitle=position, salaryrequirement=salaryrequirement)
 
 
 if __name__ == "__main__":
