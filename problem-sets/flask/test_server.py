3c597932ead8b80c7efea0349401504dfeb10014
Joanne Yeung <jttyeung@gmail.com>
2017-02-04 16:07:14 -0800

    added integration tests

--- problem-sets/flask/test_server.py
+++ problem-sets/flask/test_server.py
@@ -1,19 +1,33 @@
-import unitttest
+import unittest
 import server
 
 class KittehServerTest(unittest.TestCase):
-  """Tests for Kitteh site"""
+    """Tests for Kitteh site"""
 
-  def setUp(self):
-    """Sets up test client per route"""
-    self.client = server.app.test_client()
-    server.app.config['TESTING'] = True
+    def setUp(self):
+        """Sets up test client per route."""
+        self.client = server.app.test_client()
+        server.app.config['TESTING'] = True
 
-  def test_homepage(self):
-    """Test the homepage"""
-    result = client.get('/')
-    self.assertEqual(result.status_code, 200)
-    self.assertIn('We\'re hiring!', result.data)
+    def test_homepage(self):
+        """Test that the homepage loads."""
+        result = self.client.get('/')
+        self.assertEqual(result.status_code, 200)
+        self.assertIn('We\'re hiring!', result.data)
+
+    def test_application_form(self):
+        """Test that the application form shows."""
+        result = self.client.get('/application-form')
+        self.assertEqual(result.status_code, 200)
+        self.assertIn('Please fill out this application form to apply to an open position at Kitteh.', result.data)
+        self.assertIn('<input id="salaryrequirement" type="number" name="salaryreq" min="0" required>', result.data)
+
+    def test_application_success(self):
+        """Test that the success page upon application submission works."""
+        result = self.client.post('/application-success', data={'firstname': 'Joanne', 'lastname': 'Yeung', 'salaryreq': 1000000, 'position': 'kittehmaster'})
+        self.assertEqual(result.status_code, 200)
+        self.assertIn('We have recorded your minimum salary', result.data)
+
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
